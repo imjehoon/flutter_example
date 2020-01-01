3d74955d4c160b5c34fd7d60fa317acaf2afba0d
imjehoon_mac <imjehoon@naver.com>
2020-01-01 22:41:48 +0900

    Aligning widgets 진행 예정

--- layout_app/lib/main.dart
+++ layout_app/lib/main.dart
@@ -9,9 +9,65 @@ var stars = Row(
       Icon(Icons.star, color: Colors.green[500]),
       Icon(Icons.star, color: Colors.green[500]),
       Icon(Icons.star, color: Colors.green[500]),
-      Icon(Icons.star, color: Colors.green[500]),
+      Icon(Icons.star, color: Colors.black),
     ],
   );
+
+
+  final descTextStyle = TextStyle(
+   color: Colors.black,
+   fontWeight: FontWeight.w800,
+   fontFamily: 'Roboto',
+   letterSpacing: 0.5,
+   fontSize: 18,
+   height: 2,
+ );
+
+ 
+
+ final iconList = DefaultTextStyle.merge(
+   style: descTextStyle,
+   child: Container(
+     padding: EdgeInsets.all(20),
+     child: Row(
+       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+       children: <Widget>[
+         Column(
+           children: <Widget>[
+             Icon(Icons.kitchen, color: Colors.green[500]),
+             Text('PREP:'),
+             Text('25 min'),
+           ],
+         ),
+         Column(
+           children: <Widget>[
+             Icon(Icons.timer, color: Colors.green[500]),
+             Text('COOK:'),
+             Text('1 min'),
+           ],
+         ),
+         Column(
+           children: <Widget>[
+             Icon(Icons.restaurant, color: Colors.green[500]),
+             Text('FEEDS:'),
+             Text('4-6'),
+           ],
+         )
+       ],
+     )
+   )
+
+ );
+
+final titleText = Container(
+  padding: EdgeInsets.all(20),
+  child: Text('Strwberry Pavlova'),
+);
+
+final subitleText = Container(
+  padding: EdgeInsets.all(20),
+  child: Text('이건서브타이틀이건서브타이틀이건서브타이틀이건서브타이틀이건서브타이틀이건서브타이틀이건서브타이틀이건서브타이틀이건서브타이틀이건서브타이틀이건서브타이틀이건서브타이틀이건서브타이틀이건서브타이틀이건서브타이틀이건서브타이틀이건서브타이틀')
+);
 
  final ratings = Container(
     padding: EdgeInsets.all(20),
@@ -32,7 +88,22 @@ var stars = Row(
       ],
     ),
   );
+
 class MyApp extends StatelessWidget {
+
+ 
+
+  final leftColumn = Container(
+  padding: EdgeInsets.fromLTRB(20, 30, 20, 20),
+  child: Column(
+    children: [
+      titleText,
+      subitleText,
+      ratings,
+      iconList,
+    ],
+  ),
+);
 
   @override
   Widget build(BuildContext context) {
@@ -43,7 +114,20 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('flutter layout demo'),
         ),
-        body:ratings,
+        body: Container(
+          child: Card(
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  width: 440,
+                  child: leftColumn,
+                ),
+
+              ],
+            ),
+          )
+        )
       ),
     );
   }
